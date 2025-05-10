402bcf0a18c7dde4714cbdea273a637e518cbf34
Reyaz Ahmad <reyazahmad40544@gmail.com>
2025-05-10 12:42:52 +0530

    Second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import DoctorsPage from './components/DoctorsPage';
 import SpecialitiesPage from './components/Specialities';
-import './App.css';
+
 import Header from './components/Header';
 import AddDoctorForm from './components/AddDoctorForm';
 
